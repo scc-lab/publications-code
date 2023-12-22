22914b094db25ea3e43fb3dce4c6824f170df634
Rushikesh Kamalapurkar <rlkamalapurkar@gmail.com>
2023-12-22 13:52:43 -0600

    Code cleanup

--- 2024-TMLR-KernelPerspective/Examples/CylinderFlow/cylinderFlowKoopmanDMD.m
+++ 2024-TMLR-KernelPerspective/Examples/CylinderFlow/cylinderFlowKoopmanDMD.m
@@ -5,36 +5,29 @@
 % © Rushikesh Kamalapurkar and Joel Rosenfeld
 %
 function cylinderFlowKoopmanDMD()
-	
+%% Set up paths
 DATAPATH = '../../DATA';
 addpath('../../lib')
 
 %% Kernel selection
-% mu = 5;
-% l = 25e-12; % Gram matrix regularization parameter
 mu = 1;
-l = 0; % Gram matrix regularization parameter
 K = KernelRKHS('Gaussian',mu); 
 
 %% Load and format data for DMD
 load([DATAPATH '/FLUIDS/CYLINDER_ALL.mat']);
-DATA = VORTALL/max(vecnorm(VORTALL)); % Using normalized vorticity data to compare with the DMD book
+DATA = VORTALL/max(vecnorm(VORTALL)); % Using normalized vorticity data
 Length = 30; % Number of snapshots used for DMD. Should be between 1 and
              % 150, cannot be 150 if PlotPrediction is ON.
 h = 0.02; % Time step
-Width = 449; % Width needed for plotting only
-
-Dimension = size(DATA,1);
 % Input values X
 X = DATA(:,1:Length);
 % Output values Y = F(X)
 Y = DATA(:,2:Length+1);
 
 %% Kernel DMD
-tic
 [~,~,~,~,dr,~] = KoopmanDMD(X,Y,K,h,l);
 [~,~,~,~,drW,~] = WilliamsKDMD(X,Y,K,h);
-toc
+
 %% Plots for the paper
 reconstructionError = zeros(size(DATA,2)-1,1);
 reconstructionErrorWilliams = zeros(size(DATA,2)-1,1);
@@ -47,6 +40,9 @@ for i=1:numel(reconstructionError)
     reconstructionErrorWilliams(i) = norm(DATA(:,i+1) - drW(i,xW));
 end
 plot(reconstructionError);hold on;plot(reconstructionErrorWilliams);hold off;
+legend("Gonzalez et al.","Williams et al.");
+xlabel("t [s]");
+ylabel("Norm of the relative reconstruction error")
 reconstructionError = [time reconstructionError];
 save('KoopmanError.dat','reconstructionError','-ascii');
 reconstructionErrorWilliams = [time reconstructionErrorWilliams];
